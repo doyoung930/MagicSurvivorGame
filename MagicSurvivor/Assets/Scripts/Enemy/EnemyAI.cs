f1d462b50d5daa081a77146e68006e5f2f441931
doyoung930 <doyoung930@gmail.com>
2025-01-19 00:59:52 +0900

    EnemyHealth 추가

--- MagicSurvivor/Assets/Scripts/Enemy/EnemyAI.cs
+++ MagicSurvivor/Assets/Scripts/Enemy/EnemyAI.cs
@@ -6,15 +6,26 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float enemySpeed = 5f;
     NavMeshAgent navMeshAgent;
+    private EnemyHealth health;
+    
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        health = GetComponent<EnemyHealth>();
         target = GameObject.FindWithTag("Player").transform;
+
+        //navMeshAgent.speed = enemySpeed;
     }
 
     void Update()
     {
+        if (health.IsDead())
+        {
+            enabled = false;
+            navMeshAgent.enabled = false;
+        }
         navMeshAgent.SetDestination(target.position);
     }
 }
---
+++ MagicSurvivor/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hp = 100f;
+
+    private bool isDead = false;
+    
+    
+    public bool IsDead () { return isDead; }
+    
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0){
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        //GetComponent<Animator>().SetTrigger("die");
+    }
+}
