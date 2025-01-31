2a1cd7efb0ecb88721c188e2969cc925f907355b
doyoung930 <doyoung930@gmail.com>
2025-01-31 23:09:37 +0900

    랜덤하게 6개의 카드중 3개 뽑는 스크립트 추가

--- MagicSurvivor/Assets/RandomCardSelector.cs
+++ MagicSurvivor/Assets/RandomCardSelector.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomCardSelector : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    
+    public Sprite[] images;
+    public Image image1;
+    public Image image2;
+    public Image image3;
+    
+    public void Upgrade()
     {
+        List<Sprite> selectedImages = GetRandomImages(images, 3);
         
+        // 하위 UI의 이미지 변경
+        image1.sprite = selectedImages[0];
+        image2.sprite = selectedImages[1];
+        image3.sprite = selectedImages[2];
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    List<Sprite> GetRandomImages(Sprite[] sourceImages, int numberOfImages)
     {
-        
+        List<Sprite> imageList = new List<Sprite>(sourceImages);
+        List<Sprite> selectedImages = new List<Sprite>();
+
+        for (int i = 0; i < numberOfImages; i++)
+        {
+            if (imageList.Count == 0) break;
+
+            int randomIndex = Random.Range(0, imageList.Count);
+            selectedImages.Add(imageList[randomIndex]);
+            imageList.RemoveAt(randomIndex);
+        }
+
+        return selectedImages;
     }
 }
